097b341d004f1476eb13248094cfecd5791fbfdd
Paresh P <pareshprince34@gmail.com>
2024-10-04 12:19:29 +0530

    Added explanation comments in merge sort algorithm

--- src/sorting/MergeSort.java
+++ src/sorting/MergeSort.java
@@ -22,14 +22,16 @@ public class MergeSort {
     private static void mergeSort(int[] array) {
         // Devide the array into tow half recursively
         int arrayLength = array.length;
+        // Divide an array until the array has only one element
         if (arrayLength < 2) {
             return;
         }
         int midIndex = arrayLength / 2;
-        
+
         int[] leftHalf = new int[midIndex];
         int[] rightHalf = new int[arrayLength - midIndex];
 
+        // Fill-up arrays
         for (int i = 0; i < midIndex; i++) {
             leftHalf[i] = array[i];
         }
@@ -44,22 +46,33 @@ public class MergeSort {
         merge(array, leftHalf, rightHalf);
     }
 
+    /**
+     * Merge two sorted arrays. The strategy here is to get an element from the
+     * begining of leftHalf and compare it with the rightHalf, whichever is less put
+     * that in the result array
+     * 
+     * @param array     - Result array
+     * @param leftHalf  - Already sorted left-half
+     * @param rightHalf - Already sorted right-half
+     */
     private static void merge(int[] array, int[] leftHalf, int[] rightHalf) {
         int leftHalfLength = leftHalf.length;
         int rightHalfLength = rightHalf.length;
 
         int i = 0, j = 0, k = 0;
+        // Take the element from the left half and compare it with the right half
         while (i < leftHalfLength && j < rightHalfLength) {
-            if (leftHalf[i] < rightHalf[j]) {
+            if (leftHalf[i] < rightHalf[j]) { // Left half is less
                 array[k] = leftHalf[i];
                 i++;
-            } else {
+            } else { // Right half is less
                 array[k] = rightHalf[j];
                 j++;
             }
             k++;
         }
 
+        // Check for any left overs from left half and right half
         while (i < leftHalfLength) {
             array[k] = leftHalf[i];
             i++;
